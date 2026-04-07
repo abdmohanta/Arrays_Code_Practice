5e28e7c593d4498842c62b83fb14c93191c0ca1b
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-07 22:28:31 +0530

    Array

---
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray6.java
@@ -0,0 +1,23 @@
+package com.debasish.arraypractice.oneDimensionalArray;
+
+import java.util.Arrays;
+
+public class OneDimensionalArray6 {
+
+    public static void main(String[] args) {
+
+        // ======================================
+        // PROBLEM 7: SORT ARRAY
+        // ======================================
+
+        // Step 1: create array
+        int[] arr = {5, 2, 8, 1, 3};
+
+        // Step 2: sort array
+        Arrays.sort(arr);
+
+        // Step 3: print sorted array
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+
+    }
+}
