0d81bda8a10fe7d319c083752dc10df38f6ac42f
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-07 22:16:13 +0530

    Array4

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray4.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray4.java
@@ -1,29 +1,29 @@
-package com.debasish.arraypractice.oneDimensionalArray;
+package com.debasish.arraypractice;
 
-public class OneDimensionalArray2 {
+public class OneDimensionalArray4 {
 
     public static void main(String[] args) {
 
         // ======================================
-        // PROBLEM 3: FIND MAX ELEMENT
+        // PROBLEM 4: FIND MIN ELEMENT
         // ======================================
 
         // Step 1: create array
         int[] arr = {10, 25, 5, 40, 15};
 
-        // Step 2: assume first element is max
-        int max = arr[0];
+        // Step 2: assume first element is min
+        int min = arr[0];
 
         // Step 3: compare with other elements
         for (int i = 1; i < arr.length; i++) {
 
-            if (arr[i] > max) {
-                max = arr[i]; // update max
+            if (arr[i] < min) {
+                min = arr[i]; // update min
             }
         }
 
         // Step 4: print result
-        System.out.println("Maximum Element: " + max);
+        System.out.println("Minimum Element: " + min);
 
     }
 }
