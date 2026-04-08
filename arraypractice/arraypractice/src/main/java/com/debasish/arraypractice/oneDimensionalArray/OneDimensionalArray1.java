8d3c2869d0e7401ffd9152008237edb1f3a5317b
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-08 21:22:33 +0530

    Comment Added or removed

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray1.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray1.java
@@ -3,10 +3,7 @@ package com.debasish.arraypractice.oneDimensionalArray;
 public class OneDimensionalArray1 {
 
     public static void main(String[] args) {
-
-        // ======================================
         // PROBLEM 2: SUM OF ARRAY
-        // ======================================
         int[] arr = {10, 20, 30, 40, 50};
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
