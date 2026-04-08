33369221a18248e6e6631a1b08862447c128e298
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-08 21:11:05 +0530

    Refactor min element finding logic in OneDimensionalArray4

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray4.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray4.java
@@ -7,22 +7,15 @@ public class OneDimensionalArray4 {
         // ======================================
         // PROBLEM 4: FIND MIN ELEMENT
         // ======================================
-
-        // Step 1: create array
         int[] arr = {10, 25, 5, 40, 15};
-
-        // Step 2: assume first element is min
         int min = arr[0];
-
-        // Step 3: compare with other elements
         for (int i = 1; i < arr.length; i++) {
 
             if (arr[i] < min) {
-                min = arr[i]; // update min
+                min = arr[i];
             }
         }
 
-        // Step 4: print result
         System.out.println("Minimum Element: " + min);
 
     }
