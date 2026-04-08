48aed855992f4d0aa8e6e83c7cb6f2004105ef4f
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-08 21:09:22 +0530

    Refactor max element finding logic in OneDimensionalArray2

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray2.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray2.java
@@ -7,22 +7,14 @@ public class OneDimensionalArray2 {
         // ======================================
         // PROBLEM 3: FIND MAX ELEMENT
         // ======================================
-
-        // Step 1: create array
         int[] arr = {10, 25, 5, 40, 15};
-
-        // Step 2: assume first element is max
         int max = arr[0];
-
-        // Step 3: compare with other elements
         for (int i = 1; i < arr.length; i++) {
 
             if (arr[i] > max) {
-                max = arr[i]; // update max
+                max = arr[i];
             }
         }
-
-        // Step 4: print result
         System.out.println("Maximum Element: " + max);
 
     }
