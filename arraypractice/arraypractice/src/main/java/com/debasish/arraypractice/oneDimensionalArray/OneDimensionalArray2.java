ace239436f472e1556bb4bf2c195a88b3c9dbfb1
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-08 21:23:52 +0530

    Addddddḍ comments

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray2.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray2.java
@@ -3,10 +3,7 @@ package com.debasish.arraypractice.oneDimensionalArray;
 public class OneDimensionalArray2 {
 
     public static void main(String[] args) {
-
-        // ======================================
         // PROBLEM 3: FIND MAX ELEMENT
-        // ======================================
         int[] arr = {10, 25, 5, 40, 15};
         int max = arr[0];
         for (int i = 1; i < arr.length; i++) {
