2a292146daa8384c64a2ea5a83df673c0adc2ecf
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-08 21:11:50 +0530

    Simplify array reversal code

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray5.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray5.java
@@ -9,19 +9,13 @@ public class OneDimensionalArray5 {
         // ======================================
         // PROBLEM 6: REVERSE ARRAY
         // ======================================
-
-        // Step 1: create array
         int[] arr = {1, 2, 3, 4, 5};
-
-        // Step 2: reverse using swapping
         for (int i = 0; i < arr.length / 2; i++) {
 
-            int temp = arr[i]; // store first
-            arr[i] = arr[arr.length - 1 - i]; // swap with last
+            int temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
             arr[arr.length - 1 - i] = temp;
         }
-
-        // Step 3: print reversed array
         System.out.println("Reversed Array: " + Arrays.toString(arr));
 
     }
