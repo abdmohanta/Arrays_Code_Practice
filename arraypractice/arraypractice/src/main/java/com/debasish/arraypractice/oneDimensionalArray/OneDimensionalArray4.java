105f540e19a0fefa845fbb8d2e503d86ed773bda
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-08 21:28:44 +0530

    Comments Added

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray4.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray4.java
@@ -3,14 +3,10 @@ package com.debasish.arraypractice.oneDimensionalArray;
 public class OneDimensionalArray4 {
 
     public static void main(String[] args) {
-
-        // ======================================
         // PROBLEM 4: FIND MIN ELEMENT
-        // ======================================
         int[] arr = {10, 25, 5, 40, 15};
         int min = arr[0];
         for (int i = 1; i < arr.length; i++) {
-
             if (arr[i] < min) {
                 min = arr[i];
             }
