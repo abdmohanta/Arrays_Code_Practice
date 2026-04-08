044dad4ff67b23e2034c2760874173e3c8ff2438
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-08 21:08:39 +0530

    Refactor OneDimensionalArray1.java by removing comments
    Removed comments for clarity in the sum of array calculation.

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray1.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray1.java
@@ -7,19 +7,11 @@ public class OneDimensionalArray1 {
         // ======================================
         // PROBLEM 2: SUM OF ARRAY
         // ======================================
-
-        // Step 1: create array
         int[] arr = {10, 20, 30, 40, 50};
-
-        // Step 2: take sum variable
         int sum = 0;
-
-        // Step 3: loop and add elements
         for (int i = 0; i < arr.length; i++) {
             sum = sum + arr[i];
         }
-
-        // Step 4: print result
         System.out.println("Sum of Array: " + sum);
 
     }
