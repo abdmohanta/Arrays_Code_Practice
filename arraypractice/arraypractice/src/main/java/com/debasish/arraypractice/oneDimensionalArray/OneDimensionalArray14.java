90b62089fcd71042a600f3788d0e65dcf6475517
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-10 20:05:20 +0530

    Comment removed

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray14.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray14.java
@@ -3,10 +3,7 @@ import java.util.Arrays;
 public class OneDimensionalArray14 {
 
     public static void main(String[] args) {
-
-        // ======================================
         // PROBLEM 15: RIGHT ROTATE ARRAY
-        // ======================================
         int[] arr = {1, 2, 3, 4, 5};
         int last = arr[arr.length - 1];
         for (int i = arr.length - 1; i > 0; i--) {
