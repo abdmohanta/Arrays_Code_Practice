ce7872bf72a3d1d1520e244328af6444d31b9d1b
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-10 21:31:54 +0530

    Comment removed

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray5.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray5.java
@@ -1,19 +1,14 @@
 package com.debasish.arraypractice.oneDimensionalArray;
-
 import java.util.Arrays;
-
 public class OneDimensionalArray5 {
-
     public static void main(String[] args) {
         // PROBLEM 6: REVERSE ARRAY
         int[] arr = {1, 2, 3, 4, 5};
         for (int i = 0; i < arr.length / 2; i++) {
-
             int temp = arr[i];
             arr[i] = arr[arr.length - 1 - i];
             arr[arr.length - 1 - i] = temp;
         }
         System.out.println("Reversed Array: " + Arrays.toString(arr));
-
     }
 }
