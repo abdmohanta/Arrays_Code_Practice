e8e2a1195d1d9cc63a295b171fe7604ce0358833
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-10 19:56:00 +0530

    Done the changes

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray14.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray14.java
@@ -1,7 +1,5 @@
 package com.debasish.arraypractice.oneDimensionalArray;
-
 import java.util.Arrays;
-
 public class OneDimensionalArray14 {
 
     public static void main(String[] args) {
@@ -9,22 +7,13 @@ public class OneDimensionalArray14 {
         // ======================================
         // PROBLEM 15: RIGHT ROTATE ARRAY
         // ======================================
-
-        // Step 1: create array
         int[] arr = {1, 2, 3, 4, 5};
-
-        // Step 2: store last element
         int last = arr[arr.length - 1];
-
-        // Step 3: shift elements to right
         for (int i = arr.length - 1; i > 0; i--) {
             arr[i] = arr[i - 1];
         }
 
-        // Step 4: put last element at first
         arr[0] = last;
-
-        // Step 5: print result
         System.out.println("After Right Rotation: " + Arrays.toString(arr));
 
     }
