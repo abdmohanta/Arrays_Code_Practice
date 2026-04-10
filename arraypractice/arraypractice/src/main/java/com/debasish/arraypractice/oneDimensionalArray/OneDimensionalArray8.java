d213d452b79555a8ad347b9f69344a89428ba205
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-10 21:24:55 +0530

    Comment removed

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray8.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray8.java
@@ -1,32 +1,18 @@
 package com.debasish.arraypractice.oneDimensionalArray;
-
 public class OneDimensionalArray8 {
     public static void main(String[] args) {
-
-        // ======================================
         // PROBLEM 9: COUNT EVEN & ODD
-        // ======================================
-
-        // Step 1: create array
         int[] arr = {10, 15, 20, 25, 30};
-
-        // Step 2: counters
         int even = 0;
         int odd = 0;
-
-        // Step 3: loop and check
         for (int i = 0; i < arr.length; i++) {
-
             if (arr[i] % 2 == 0) {
-                even++;   // even count
+                even++;
             } else {
-                odd++;    // odd count
+                odd++;
             }
         }
-
-        // Step 4: print result
         System.out.println("Even Count: " + even);
         System.out.println("Odd Count: " + odd);
-
     }
 }
