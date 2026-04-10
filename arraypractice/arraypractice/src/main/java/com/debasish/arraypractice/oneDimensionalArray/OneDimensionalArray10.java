4e4354eb9a799b88f8384b77920c00568513b52f
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-10 21:20:08 +0530

    Comment removed

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray10.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray10.java
@@ -1,34 +1,17 @@
 package com.debasish.arraypractice.oneDimensionalArray;
-
 import java.util.Arrays;
-
 public class OneDimensionalArray10 {
-
     public static void main(String[] args) {
-
-        // ======================================
         // PROBLEM 11: MERGE TWO ARRAYS
-        // ======================================
-
-        // Step 1: create two arrays
         int[] a = {1, 2, 3};
         int[] b = {4, 5, 6};
-
-        // Step 2: create new array
         int[] merge = new int[a.length + b.length];
-
-        // Step 3: copy first array
         for (int i = 0; i < a.length; i++) {
             merge[i] = a[i];
         }
-
-        // Step 4: copy second array
         for (int i = 0; i < b.length; i++) {
             merge[a.length + i] = b[i];
         }
-
-        // Step 5: print merged array
         System.out.println("Merged Array: " + Arrays.toString(merge));
-
     }
 }
