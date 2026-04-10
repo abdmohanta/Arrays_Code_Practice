a20ebb96535c62b3a12c89c0f71845d324e7cde4
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-10 21:36:21 +0530

    Comment removed

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray16.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray16.java
@@ -3,23 +3,18 @@ import java.util.Arrays;
 // PROBLEM 17: MOVE ZEROS TO END OF ARRAY
 public class OneDimensionalArray16 {
     public static void main(String[] args) {
-
         int[] arr = {0, 1, 0, 3, 12};
         int j = 0;
         for (int i = 0; i < arr.length; i++) {
-
             if (arr[i] != 0) {
                 arr[j] = arr[i];
                 j++;
             }
         }
-
         while (j < arr.length) {
             arr[j] = 0;
             j++;
         }
-
         System.out.println("After Moving Zeros: " + Arrays.toString(arr));
-
     }
 }
