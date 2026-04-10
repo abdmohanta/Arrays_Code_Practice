02341f8590a07d9f149aa1a9fab0a5de7f611bc8
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-10 21:21:37 +0530

    Comment removed

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray9.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray9.java
@@ -1,28 +1,13 @@
 package com.debasish.arraypractice.oneDimensionalArray;
-
 import java.util.Arrays;
-
 public class OneDimensionalArray9 {
-
     public static void main(String[] args) {
-
-        // ======================================
         // PROBLEM 10: COPY ARRAY
-        // ======================================
-
-        // Step 1: create original array
         int[] arr = {10, 20, 30, 40, 50};
-
-        // Step 2: create new array of same size
         int[] copy = new int[arr.length];
-
-        // Step 3: copy elements manually
         for (int i = 0; i < arr.length; i++) {
             copy[i] = arr[i];
         }
-
-        // Step 4: print copied array
         System.out.println("Copied Array: " + Arrays.toString(copy));
-
     }
 }
