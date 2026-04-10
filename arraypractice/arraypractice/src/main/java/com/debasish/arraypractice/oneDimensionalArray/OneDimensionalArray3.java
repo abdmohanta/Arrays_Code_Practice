057f25cf65ec6a756bc2b6bb308fe3d6c211fb18
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-10 21:28:00 +0530

    Comment removed

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray3.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray3.java
@@ -1,14 +1,11 @@
 package com.debasish.arraypractice.oneDimensionalArray;
-
 public class OneDimensionalArray3 {
     // PROBLEM 5: SEARCH ELEMENT
     public static void main(String[] args) {
-
         int[] arr = {10, 20, 30, 40, 50};
         int key = 30;
         boolean found = false;
         for (int i = 0; i < arr.length; i++) {
-
             if (arr[i] == key) {
                 found = true;
                 break;
