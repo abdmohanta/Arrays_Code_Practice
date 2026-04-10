8c500a63ba7875fe2f885ccac0ab6d113df21a8a
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-10 21:27:11 +0530

    Comment removed

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray7.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray7.java
@@ -1,34 +1,19 @@
 package com.debasish.arraypractice.oneDimensionalArray;
-
 public class OneDimensionalArray7 {
-
     public static void main(String[] args) {
-
-        // ======================================
         // PROBLEM 8: SECOND LARGEST ELEMENT
-        // ======================================
-
-        // Step 1: create array
         int[] arr = {10, 20, 30, 40, 50};
-
-        // Step 2: take two variables
         int first = Integer.MIN_VALUE;
         int second = Integer.MIN_VALUE;
-
-        // Step 3: loop through array
         for (int i = 0; i < arr.length; i++) {
-
             if (arr[i] > first) {
-                second = first;     // old max becomes second
-                first = arr[i];     // update max
+                second = first;
+                first = arr[i];
             }
             else if (arr[i] > second && arr[i] != first) {
-                second = arr[i];    // update second max
+                second = arr[i];
             }
         }
-
-        // Step 4: print result
         System.out.println("Second Largest: " + second);
-
     }
 }
