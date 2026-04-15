067fa87c52325b064a4c4851aa8bbfd8f7c48fbd
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-15 21:10:04 +0530

    Added new problem code

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray38.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray38.java
@@ -1,10 +1,8 @@
 package com.debasish.arraypractice.oneDimensionalArray;
 public class OneDimensionalArray38 {
-
     public static void main(String[] args) {
         // PROBLEM 39: MAX PRODUCT SUBARRAY
         int[] arr = {2, 3, -2, 4};
-
         int mxProduct = arr[0];
         int currentMax = arr[0];
         int currentMin = arr[0];
@@ -12,10 +10,8 @@ public class OneDimensionalArray38 {
             int temp = currentMax;
             currentMax = Math.max(arr[i],
                     Math.max(currentMax * arr[i], currentMin * arr[i]));
-
             currentMin = Math.min(arr[i],
                     Math.min(temp * arr[i], currentMin * arr[i]));
-
             if (currentMax > maxProduct) {
                 maxProduct = currentMax;
             }
