11150183903d69576693c36c094e25ca73181edf
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-17 23:07:08 +0530

    Added new problem code

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray43.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray43.java
@@ -1,37 +1,19 @@
 package com.debasish.arraypractice.oneDimensionalArray;
-
 public class OneDimensionalArray43 {
-
     public static void main(String[] args) {
-
         // PROBLEM 44: MAX SUBARRAY SUM (BRUTE)
-        // ======================================
-
-        // Step 1: create array
         int[] arr = {1, -2, 3, 4};
-
         int n = arr.length;
-
         int maxSum = Integer.MIN_VALUE;
-
-        // Step 2: generate all subarrays
         for (int start = 0; start < n; start++) {
-
             int currentSum = 0;
-
             for (int end = start; end < n; end++) {
-
                 currentSum = currentSum + arr[end];
-
-                // update max sum
                 if (currentSum > maxSum) {
                     maxSum = currentSum;
                 }
             }
         }
-
-        // Step 3: print result
         System.out.println("Maximum Subarray Sum: " + maxSum);
-
     }
 }
