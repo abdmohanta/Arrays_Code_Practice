11523a5a284c078d2e7c8482df5ff9b564ae4bf6
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-17 22:45:16 +0530

    Added new problem code

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray41.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray41.java
@@ -1,26 +1,16 @@
 package com.debasish.arraypractice.oneDimensionalArray;
-
 public class OneDimensionalArray41 {
-
     public static void main(String[] args) {
         // PROBLEM 42: PRINT ALL SUBARRAYS
         int[] arr = {1, 2, 3};
-
         int n = arr.length;
-
-        // Step 2: generate subarrays
         for (int start = 0; start < n; start++) {
-
             for (int end = start; end < n; end++) {
-
-                // print subarray from start to end
                 for (int i = start; i <= end; i++) {
                     System.out.print(arr[i] + " ");
                 }
-
-                System.out.println(); // new line
+                System.out.println();
             }
         }
-
     }
 }
