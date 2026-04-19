9c39f735a239237c6e562de637e5f07985c4165e
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-19 21:20:44 +0530

    Added new problem code

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray51.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray51.java
@@ -1,39 +1,20 @@
 package com.debasish.arraypractice.oneDimensionalArray;
-
 import java.util.Arrays;
-
 public class OneDimensionalArray51 {
-
     public static void main(String[] args) {
-
-        // ======================================
         // PROBLEM 52: NEXT GREATER ELEMENT
-        // ======================================
-
-        // Step 1: create array
         int[] arr = {4, 5, 2, 10};
-
         int n = arr.length;
-
-        // Step 2: result array
         int[] result = new int[n];
-
-        // Step 3: find next greater
         for (int i = 0; i < n; i++) {
-
-            result[i] = -1; // default
-
+            result[i] = -1;
             for (int j = i + 1; j < n; j++) {
-
                 if (arr[j] > arr[i]) {
                     result[i] = arr[j];
                     break;
                 }
             }
         }
-
-        // Step 4: print result
         System.out.println("Next Greater Elements: " + Arrays.toString(result));
-
     }
 }
