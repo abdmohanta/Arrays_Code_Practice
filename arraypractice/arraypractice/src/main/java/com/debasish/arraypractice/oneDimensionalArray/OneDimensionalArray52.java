fec618ed5e8cce80cc07ef85ef32bb2bec1b5645
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-19 21:24:21 +0530

    Added new problem code

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray52.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray52.java
@@ -1,29 +1,14 @@
 package com.debasish.arraypractice.oneDimensionalArray;
-
 import java.util.Arrays;
-
 public class OneDimensionalArray52 {
-
     public static void main(String[] args) {
-
-        // ======================================
         // PROBLEM 53: STOCK SPAN
-        // ======================================
-
-        // Step 1: create array
         int[] price = {100, 80, 60, 70, 60, 75, 85};
-
         int n = price.length;
-
         int[] span = new int[n];
-
-        // Step 2: calculate span
         for (int i = 0; i < n; i++) {
-
-            span[i] = 1; // minimum span
-
+            span[i] = 1;
             for (int j = i - 1; j >= 0; j--) {
-
                 if (price[j] <= price[i]) {
                     span[i]++;
                 } else {
@@ -31,9 +16,6 @@ public class OneDimensionalArray52 {
                 }
             }
         }
-
-        // Step 3: print result
         System.out.println("Stock Span: " + Arrays.toString(span));
-
     }
 }
