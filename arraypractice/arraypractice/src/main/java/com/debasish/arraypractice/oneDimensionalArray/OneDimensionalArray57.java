b18336b73da8d5bea4be9ea7a2fca06f8c55dc27
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-22 21:15:33 +0530

    Added new problem code

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray57.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray57.java
@@ -1,38 +1,20 @@
 package com.debasish.arraypractice.oneDimensionalArray;
-
 public class OneDimensionalArray57 {
-
     public static void main(String[] args) {
-
-        // ======================================
         // PROBLEM 58: SUBARRAY SUM EQUALS K (COUNT)
-        // ======================================
-
-        // Step 1: create array
         int[] arr = {1, 1, 1};
-
         int k = 2;
         int n = arr.length;
-
         int count = 0;
-
-        // Step 2: check all subarrays
         for (int start = 0; start < n; start++) {
-
             int sum = 0;
-
             for (int end = start; end < n; end++) {
-
                 sum = sum + arr[end];
-
                 if (sum == k) {
                     count++;
                 }
             }
         }
-
-        // Step 3: print result
         System.out.println("Total Subarrays: " + count);
-
     }
 }
