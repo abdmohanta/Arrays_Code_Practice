54912c3c9cbad416772fdbdecbd4ec605825fc51
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-23 22:57:15 +0530

    Added new problem code

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray62.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray62.java
@@ -1,28 +1,13 @@
 package com.debasish.arraypractice.oneDimensionalArray;
-
 import java.util.Arrays;
-
 public class OneDimensionalArray62 {
-
     public static void main(String[] args) {
-
-        // ======================================
         // PROBLEM 63: K-th LARGEST ELEMENT
-        // ======================================
-
-        // Step 1: create array
         int[] arr = {3, 2, 1, 5, 6, 4};
-
         int k = 2;
         int n = arr.length;
-
-        // Step 2: sort array
         Arrays.sort(arr);
-
-        // Step 3: find k-th largest
         int result = arr[n - k];
-
-        // Step 4: print result
         System.out.println("K-th Largest Element: " + result);
 
     }
