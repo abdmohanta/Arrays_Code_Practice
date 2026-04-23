57295616f5166ee93acac1e5c8e1a406864d0447
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-23 22:52:07 +0530

    Added new problem code

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray61.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray61.java
@@ -1,31 +1,16 @@
 package com.debasish.arraypractice.oneDimensionalArray;
 public class OneDimensionalArray61 {
-
     public static void main(String[] args) {
-
-        // ======================================
         // PROBLEM 62: LIS (BINARY SEARCH)
-        // ======================================
-
-        // Step 1: create array
         int[] arr = {10, 9, 2, 5, 3, 7, 101, 18};
-
         int n = arr.length;
-
         int[] tail = new int[n];
         int size = 0;
-
-        // Step 2: process elements
         for (int i = 0; i < n; i++) {
-
             int num = arr[i];
-
-            // binary search
             int left = 0, right = size;
-
             while (left < right) {
                 int mid = (left + right) / 2;
-
                 if (tail[mid] < num) {
                     left = mid + 1;
                 } else {
@@ -34,14 +19,10 @@ public class OneDimensionalArray61 {
             }
 
             tail[left] = num;
-
             if (left == size) {
                 size++;
             }
         }
-
-        // Step 3: print result
         System.out.println("LIS Length (Optimized): " + size);
-
     }
 }
