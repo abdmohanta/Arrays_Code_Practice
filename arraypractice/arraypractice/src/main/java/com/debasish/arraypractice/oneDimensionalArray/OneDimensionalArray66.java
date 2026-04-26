078172bfcde9efae8fb38de5779e43d987e11f0b
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-26 14:38:42 +0530

    Added new problem code

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray66.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray66.java
@@ -1,36 +1,19 @@
 package com.debasish.arraypractice.oneDimensionalArray;
-
 public class OneDimensionalArray66 {
-
     public static void main(String[] args) {
-
-        // ======================================
         // PROBLEM 67: FIND DUPLICATE (FLOYD CYCLE)
-        // ======================================
-
-        // Step 1: create array
         int[] arr = {1, 3, 4, 2, 2};
-
-        // Step 2: initialize slow & fast
         int slow = arr[0];
         int fast = arr[0];
-
-        // Step 3: detect cycle
         do {
             slow = arr[slow];
             fast = arr[arr[fast]];
         } while (slow != fast);
-
-        // Step 4: find entry point (duplicate)
         slow = arr[0];
-
         while (slow != fast) {
             slow = arr[slow];
             fast = arr[fast];
         }
-
-        // Step 5: print result
         System.out.println("Duplicate Number: " + slow);
-
     }
 }
