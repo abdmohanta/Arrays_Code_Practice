29a05bf62e1b838a19767f3bb61df128ac437b09
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-27 21:26:34 +0530

    Added new problem code

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray68.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray68.java
@@ -1,37 +1,20 @@
 package com.debasish.arraypractice.oneDimensionalArray;
-
 public class OneDimensionalArray68 {
-
     public static void main(String[] args) {
-
-        // ======================================
         // PROBLEM 69: FIND ALL MISSING NUMBERS
-        // ======================================
-
-        // Step 1: create array
         int[] arr = {4, 3, 2, 7, 8, 2, 3, 1};
-
         int n = arr.length;
-
-        // Step 2: mark visited using negative sign
         for (int i = 0; i < n; i++) {
-
             int index = Math.abs(arr[i]) - 1;
-
             if (arr[index] > 0) {
                 arr[index] = -arr[index];
             }
         }
-
-        // Step 3: find missing numbers
         System.out.println("Missing Numbers:");
-
         for (int i = 0; i < n; i++) {
-
             if (arr[i] > 0) {
                 System.out.println(i + 1);
             }
         }
-
     }
 }
