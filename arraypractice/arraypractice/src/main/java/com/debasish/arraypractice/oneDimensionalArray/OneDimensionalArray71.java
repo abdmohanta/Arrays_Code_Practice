66f5ab27aa1157b3d5389495d033e4a5252f1ab5
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-30 22:29:51 +0530

    Added new problem code

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray71.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray71.java
@@ -1,45 +1,29 @@
 package com.debasish.arraypractice.oneDimensionalArray;
-
 public class OneDimensionalArray71 {
-
     public static void main(String[] args) {
-
-        // ======================================
         // PROBLEM 72: MAX CONSECUTIVE ONES (1 FLIP)
-        // ======================================
-
-        // Step 1: create array
         int[] arr = {1, 0, 1, 1, 0};
-
         int left = 0;
         int zeroCount = 0;
         int maxLength = 0;
 
-        // Step 2: sliding window
         for (int right = 0; right < arr.length; right++) {
 
             if (arr[right] == 0) {
                 zeroCount++;
             }
 
-            // allow only one zero
             while (zeroCount > 1) {
-
                 if (arr[left] == 0) {
                     zeroCount--;
                 }
-
                 left++;
             }
-
             int length = right - left + 1;
-
             if (length > maxLength) {
                 maxLength = length;
             }
         }
-
-        // Step 3: print result
         System.out.println("Max Consecutive Ones (with 1 flip): " + maxLength);
 
     }
