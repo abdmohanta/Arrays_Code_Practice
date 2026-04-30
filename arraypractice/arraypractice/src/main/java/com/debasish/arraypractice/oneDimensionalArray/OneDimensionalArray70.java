629334467dc98137dd05ba25487fbcf29b7c26eb
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-04-30 21:49:08 +0530

    Added new problem code

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray70.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray70.java
@@ -1,35 +1,21 @@
 package com.debasish.arraypractice.oneDimensionalArray;
-
 public class OneDimensionalArray70 {
-
     public static void main(String[] args) {
-
-        // ======================================
         // PROBLEM 71: MAXIMUM CONSECUTIVE ONES
-        // ======================================
-
-        // Step 1: create array
         int[] arr = {1, 1, 0, 1, 1, 1};
-
         int maxCount = 0;
         int currentCount = 0;
 
-        // Step 2: traverse array
         for (int i = 0; i < arr.length; i++) {
-
             if (arr[i] == 1) {
-                currentCount++; // increase count
+                currentCount++;
             } else {
-                currentCount = 0; // reset when 0 comes
+                currentCount = 0;
             }
-
-            // update max
             if (currentCount > maxCount) {
                 maxCount = currentCount;
             }
         }
-
-        // Step 3: print result
         System.out.println("Maximum Consecutive Ones: " + maxCount);
 
     }
