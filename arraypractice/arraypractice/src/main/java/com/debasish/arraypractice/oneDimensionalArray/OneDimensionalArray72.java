37dafe04ab89d4b677679192758d7a90c3e3ad2a
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-05-01 21:53:23 +0530

    Added new problem code

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray72.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray72.java
@@ -1,30 +1,18 @@
 package com.debasish.arraypractice.oneDimensionalArray;
-
 public class OneDimensionalArray72 {
-
     public static void main(String[] args) {
-
-        // ======================================
         // PROBLEM 73: MAX CONSECUTIVE ONES (K FLIPS)
-        // ======================================
-
-        // Step 1: create array
         int[] arr = {1, 0, 1, 0, 1, 0, 1};
-
         int k = 2;
-
         int left = 0;
         int zeroCount = 0;
         int maxLength = 0;
-
-        // Step 2: sliding window
         for (int right = 0; right < arr.length; right++) {
 
             if (arr[right] == 0) {
                 zeroCount++;
             }
 
-            // shrink window if zeroCount > k
             while (zeroCount > k) {
 
                 if (arr[left] == 0) {
@@ -41,7 +29,6 @@ public class OneDimensionalArray72 {
             }
         }
 
-        // Step 3: print result
         System.out.println("Max Consecutive Ones (K flips): " + maxLength);
 
     }
