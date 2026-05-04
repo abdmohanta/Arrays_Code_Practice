74629435673c22b08c8bf2c2de106cfd4141aa41
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-05-04 20:35:04 +0530

    Added new problem code

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray80.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray80.java
@@ -1,32 +1,17 @@
 package com.debasish.arraypractice.oneDimensionalArray;
 
 import java.util.Stack;
-
 public class OneDimensionalArray80 {
-
     public static void main(String[] args) {
-
-        // ======================================
         // PROBLEM 81: VALID PARENTHESES
-        // ======================================
-
-        // Step 1: input string
         String s = "()[]{}";
-
         Stack<Character> stack = new Stack<>();
-
         boolean isValid = true;
-
-        // Step 2: process characters
         for (int i = 0; i < s.length(); i++) {
-
             char ch = s.charAt(i);
-
-            // if opening bracket
             if (ch == '(' || ch == '{' || ch == '[') {
                 stack.push(ch);
             }
-            // if closing bracket
             else {
 
                 if (stack.isEmpty()) {
@@ -46,12 +31,10 @@ public class OneDimensionalArray80 {
             }
         }
 
-        // Step 3: check result
         if (!stack.isEmpty()) {
             isValid = false;
         }
 
-        // Step 4: print result
         if (isValid) {
             System.out.println("Valid Parentheses");
         } else {
