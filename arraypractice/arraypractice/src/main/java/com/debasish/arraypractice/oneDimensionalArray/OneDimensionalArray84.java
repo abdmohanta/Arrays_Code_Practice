b42717215877a17bcaa925a60883d46c77c2f4ca
Debasish Mohanta <219381354+abdmohanta@users.noreply.github.com>
2026-05-06 21:18:24 +0530

    Added new problem code

--- arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray84.java
+++ arraypractice/arraypractice/src/main/java/com/debasish/arraypractice/oneDimensionalArray/OneDimensionalArray84.java
@@ -6,34 +6,20 @@ public class OneDimensionalArray84 {
 
     public static void main(String[] args) {
         // PROBLEM 85: ASTEROID COLLISION
-        // ======================================
-
-        // Step 1: create array
         int[] asteroids = {5, 10, -5};
-
         Stack<Integer> stack = new Stack<>();
-
-        // Step 2: process asteroids
         for (int i = 0; i < asteroids.length; i++) {
-
             int current = asteroids[i];
-
             boolean exploded = false;
-
-            // collision condition
             while (!stack.isEmpty()
                     && current < 0
                     && stack.peek() > 0) {
-
                 int top = stack.peek();
-
-                // top asteroid smaller
                 if (Math.abs(current) > top) {
                     stack.pop();
                     continue;
                 }
 
-                // equal size
                 else if (Math.abs(current) == top) {
                     stack.pop();
                 }
@@ -42,7 +28,6 @@ public class OneDimensionalArray84 {
                 break;
             }
 
-            // add asteroid if not exploded
             if (!exploded) {
                 stack.push(current);
             }
